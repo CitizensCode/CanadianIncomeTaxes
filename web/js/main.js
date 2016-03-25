92cb4c25d17344a6cc6778d1b31cfc6ede2e4a8e
Ryan Brideau <ryan.brideau@gmail.com>
2016-03-25 12:19:14 -0300

    Added all health fees

--- web/js/main.js
+++ web/js/main.js
@@ -3,29 +3,37 @@ var data;
 
 d3.json("/data/avgTax.json", function(error, data) {
 
-if (error) return console.warn(error);
+if (error) {
+  return console.warn(error);
+}
 
 nv.addGraph(function() {
-  chart = nv.models.lineChart()
+  chart = nv.models.lineWithFocusChart()
     .options({
       transitionDuration: 300,
       useInteractiveGuideline: true,
-      forceX: [5000],
-      forceY: [.18, .52],
       showLegend: true,
       xScale: d3.scale.linear()
     });
 
+  chart.brushExtent([10000,990000]);
+
   chart.xAxis
-    .axisLabel("Income")
     .tickFormat(d3.format('$,.d'));
+
+  chart.x2Axis
+    .axisLabel("Income");
 
   chart.yAxis
     .axisLabel('Average Tax (%)')
     .tickFormat(d3.format('.1%'));
 
+  chart.y2Axis
+    .tickFormat(d3.format('.1%'));
+
   d3.select('#chart1').append('svg')
     .datum(data)
+    .transition().duration(500)
     .call(chart);
 
   nv.utils.windowResize(chart.update);
